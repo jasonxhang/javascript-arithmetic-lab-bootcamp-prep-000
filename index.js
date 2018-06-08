baefa78e1f6f16dd2b94e6da6fa115aef2b1565a
Jason Hang <jasonxhang@gmail.com>
2018-06-08 16:55:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  console.log(parseInt(n,10));
+  return(parseInt(n,10));
 }
 
 function preserveDecimal(n) {
-  console.log(parseFloat(n));
+  return(parseFloat(n));
 }
 
 
